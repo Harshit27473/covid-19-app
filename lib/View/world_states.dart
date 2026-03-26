e2b1d8ac4741ce6632f4a3ae385e8bd0d4caf550
Harshit27473 <harshitduhan2004@gmail.com>
2026-03-26 15:26:26 +0530

    world states

--- lib/View/world_states.dart
+++ lib/View/world_states.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class WorldStatesScreen extends StatefulWidget {
   const WorldStatesScreen({super.key});
@@ -7,10 +8,101 @@ class WorldStatesScreen extends StatefulWidget {
   State<WorldStatesScreen> createState() => _WorldStatesScreenState();
 }
 
-class _WorldStatesScreenState extends State<WorldStatesScreen> {
+class _WorldStatesScreenState extends State<WorldStatesScreen> with TickerProviderStateMixin{
+
+  late final  AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  )..repeat();
+
+  @override
+
+  void dispose(){
+    super.dispose();
+    _controller.dispose();
+  }
+
+  final colorList = <Color>  [
+    const Color(0xff4285F4),
+    const Color(0xff1aa260),
+    const Color(0xffde5246),
+  ];
+
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.01,),
+              PieChart(
+                   dataMap: const {
+                    "Total": 20,
+                    "Recovered": 15,
+                    "Deaths": 5,
+                  },
+                chartRadius: MediaQuery.of(context).size.width / 3.2,
+                legendOptions: const LegendOptions(
+                  legendPosition: LegendPosition.left
+                ),
+                animationDuration: const Duration(milliseconds: 1200),
+                chartType: ChartType.ring,
+                colorList: colorList,
+              ),
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(vertical: MediaQuery.of(context).size.height* 0.06),
+                child: Card(
+                  child: Column(
+                        children: [
+                          ReusableRow(title:  'total', value: '200'),
+                          ReusableRow(title:  'total', value: '200'),
+                          ReusableRow(title:  'total', value: '200'),
+                  ],
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xff1aa260)
+                ),
+                child: const Center(
+                  child: Text('Track Countries'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class ReusableRow extends StatelessWidget {
+  String title, value;
+  ReusableRow({super.key , required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10,right: 10,top: 10,bottom: 5),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title),
+              Text(value),
+            ],
+          ),
+          SizedBox(height: 5,),
+          Divider()
+        ],
+      ),
     );
   }
 }
